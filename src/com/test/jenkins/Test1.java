fb2587fe6f40e469da4cd1db92916a0081c22584
Saravana-K-Jayaram <sajayaram>
2018-01-25 19:14:03 +0530

    Initial Commit

---
+++ src/com/test/jenkins/Test1.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+/**
+ * @author xecckw2
+ *
+ */
+package com.test.jenkins;
+
+public class Test1{
+
+	public static void main (String args[])
+	{
+		
+		for (int i=1; i<=10; i++)
+		{
+			 System.out.println("Hello World ... i ... :"+i);
+			
+		
+		}
+	}
+}
